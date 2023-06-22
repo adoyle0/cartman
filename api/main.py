3103d8282cfac50c51d88cdf731b0ac6da46194d
Adam <24621027+adoyle0@users.noreply.github.com>
2023-06-22 18:14:37 -0400

    docker

--- api/main.py
+++ api/main.py
@@ -3,4 +3,4 @@ import multiprocessing
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    uvicorn.run('src.chatbots:api', host='127.0.0.1', port=6969, reload=True)
+    uvicorn.run('src.chatbots:api', host='0.0.0.0', port=6969, reload=True)
